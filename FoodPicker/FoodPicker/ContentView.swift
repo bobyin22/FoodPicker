5835b9a789a2a7d8a98f928b9d5024ab1db796fc
bobyin <bobyin22@gmail.com>
2025-02-04 16:38:30 +0800

    [modify] EP4 實作第一個畫面:今天吃什麼

--- FoodPicker/FoodPicker/ContentView.swift
+++ FoodPicker/FoodPicker/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    let food = ["漢堡", "沙拉", "披薩", "義大利麵", "雞腿便當", "刀削麵", "火鍋", "牛肉麵", "關東煮"]
+    @State private var selectedFood: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:30) {
+            Image("dinner")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Text("今天吃什麼？")
+                .bold()
+            
+            if selectedFood != .none {
+                Text(selectedFood ?? "")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.green)
+            }
+            
+            Button {
+                selectedFood = food.shuffled().filter{ $0 != selectedFood }.first
+            } label: {
+                Text(selectedFood == .none ? "告訴我!" : "換一個").frame(width: 200)
+            }
+            .padding(.bottom, -15)
+            
+            Button {
+                selectedFood = .none
+            } label: {
+                Text("重置").frame(width: 200, alignment: .center)
+            }
+            .buttonStyle(.bordered)
+
         }
         .padding()
+        .frame(maxHeight: .infinity)
+        .background(Color(.secondarySystemBackground))
+        .font(.title)
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+        .animation(.easeInOut(duration: 1), value: selectedFood)
     }
 }
 
