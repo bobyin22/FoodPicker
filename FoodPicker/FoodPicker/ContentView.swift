23d410c134fc47ea3a8244403ed23dd05542aa88
Chou Po Yin <94157717+bobyin22@users.noreply.github.com>
2025-02-04 15:19:39 +0800

    init xcode file

---
+++ FoodPicker/FoodPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodPicker
+//
+//  Created by Yin Bob on 2025/2/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodPicker/FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by Yin Bob on 2025/2/4.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
