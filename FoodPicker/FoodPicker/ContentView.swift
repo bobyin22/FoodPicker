123585ab82cdb52c603f4a42caac2ed99bb4ac69
bobyin <bobyin22@gmail.com>
2025-02-05 15:53:51 +0800

    [modify] EP8 依舊是排版練習 - 依據教學改 Text 與 Divider 做表格

--- FoodPicker/FoodPicker/ContentView.swift
+++ FoodPicker/FoodPicker/ContentView.swift
@@ -13,94 +13,108 @@ struct ContentView: View {
     @State private var selectedFood: Food?
     
     var body: some View {
-        VStack(spacing:30) {
-            Group {
-                if selectedFood != .none {
-                    Text("\(selectedFood!.image)")
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.1)
-                        .lineLimit(1)
-                } else {
-                    Image("dinner")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                }
-            }.frame(height: 250)
-            
-            
-            Text("今天吃什麼？")
-                .bold()
-            
-            if selectedFood != .none {
-                HStack {
-                    Text(selectedFood!.name ?? "")
-                        .font(.largeTitle)
-                        .bold()
-                        .foregroundStyle(.green)
-                        .id(selectedFood!.name)
-                        .transition(.asymmetric(
-                            insertion: .opacity
-                                .animation(.easeInOut(duration: 0.5).delay(0.2)),
-                            removal: .opacity
-                                .animation(.easeInOut(duration: 0.4))))
-                    Image(systemName: "info.circle.fill").foregroundColor(.gray)
-                }
-                
-                
-                Text("熱量\(Int(selectedFood!.calorie))大卡")
-                
-                Grid {
-                    GridRow {
-                        Text("蛋白質")
-                        Text("脂肪")
-                        Text("碳水")
+        ScrollView {
+            VStack(spacing: 30) {
+                Group {
+                    if selectedFood != .none {
+                        Text("\(selectedFood!.image)")
+                            .font(.system(size: 200))
+                            .minimumScaleFactor(0.1)
+                            .lineLimit(1)
+                    } else {
+                        Image("dinner")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
                     }
+                }.frame(height: 250)
+                
+                
+                Text("今天吃什麼？")
                     .bold()
-                    Divider()
-                    GridRow {
-                        Text("\(Int(selectedFood!.protein))")
-                        Text("\(Int(selectedFood!.calorie))")
-                        Text("\(Int(selectedFood!.carb))")
+                
+                if selectedFood != .none {
+                    HStack {
+                        Text(selectedFood!.name ?? "")
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundStyle(.green)
+                            .id(selectedFood!.name)
+                            .transition(.asymmetric(
+                                insertion: .opacity
+                                    .animation(.easeInOut(duration: 0.5).delay(0.2)),
+                                removal: .opacity
+                                    .animation(.easeInOut(duration: 0.4))))
+                        Image(systemName: "info.circle.fill")
+                            .foregroundColor(.secondary)
                     }
-                    Divider()
+                    
+                    
+                    Text("熱量\(selectedFood!.calorie.formatted())")
+                        .font(.title2)
+                    
+                    HStack {
+                        VStack(spacing: 12) {
+                            Text("蛋白質")
+                            Text(selectedFood!.protein.formatted() + " g")
+                        }
+                        
+                        Divider().padding(.horizontal)
+                        
+                        VStack(spacing: 12) {
+                            Text("脂肪")
+                            Text(selectedFood!.calorie.formatted() + " g")
+                        }
+                        
+                        Divider().frame(width: 1).padding(.horizontal)
+                        
+                        VStack(spacing: 12) {
+                            Text("碳水")
+                            Text(selectedFood!.carb.formatted() + " g")
+                        }
+                    }
+                    .font(.title3)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 8).foregroundColor(Color(.systemBackground)))
                 }
-                .font(.title3)
-                .background(Color(.white))
-                .frame(width: 200, height: 100)
-            } else {
-                EmptyView()
+                
+                
+                Spacer().layoutPriority(1)
+                
+                Button {
+                    selectedFood = food.shuffled().first { $0 != selectedFood }
+                } label: {
+                    Text(selectedFood == .none ? "告訴我" : "換一個").frame(width: 200)
+                        .animation(.none, value: selectedFood)
+                        .transformEffect(.identity)
+                }
+                .padding(.bottom, -15)
+                
+                Button {
+                    selectedFood = .none
+                } label: {
+                    Text("重置").frame(width: 200, alignment: .center)
+                }
+                .buttonStyle(.bordered)
+                
             }
-            
-            //Spacer()
-            
-            Button {
-                selectedFood = food.shuffled().first { $0 != selectedFood }
-            } label: {
-                Text(selectedFood == .none ? "告訴我" : "換一個").frame(width: 200)
-                    .animation(.none, value: selectedFood)
-                    .transformEffect(.identity)
-            }
-            .padding(.bottom, -15)
-            
-            Button {
-                selectedFood = .none
-            } label: {
-                Text("重置").frame(width: 200, alignment: .center)
-            }
-            .buttonStyle(.bordered)
+            .padding()
+            .frame(maxWidth: .infinity, minHeight: UIScreen.main.bounds.height - 100)
+            .font(.title)
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            .animation(.easeInOut(duration: 0.6), value: selectedFood)
+        }.background(Color(.secondarySystemBackground))
+    }
+}
 
-        }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(.secondarySystemBackground))
-        .font(.title)
-        .buttonStyle(.borderedProminent)
-        .buttonBorderShape(.capsule)
-        .controlSize(.large)
-        .animation(.easeInOut(duration: 0.6), value: selectedFood)
+extension ContentView {
+    init(selectedFood: Food) {
+        _selectedFood = State(wrappedValue: selectedFood)
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(selectedFood: .examples.first!)
+    //ContentView(selectedFood: .examples.first!).previewDevice(.iPad)
 }
---
+++ FoodPicker/FoodPicker/PreviewDevice+.swift
@@ -0,0 +1,13 @@
+//
+//  PreviewDevice+.swift
+//  FoodPicker
+//
+//  Created by Yin Bob on 2025/2/5.
+//
+
+import SwiftUI
+
+extension PreviewDevice {
+    static let iPad = PreviewDevice(rawValue: "iPad Pro (12.9-inch) (6th generation)")
+    static let iPhoneSE = PreviewDevice(rawValue: "iPhone SE (3rd generation)")
+}
