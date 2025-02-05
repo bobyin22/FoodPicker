1d1a724d231031618a01d6ea140ddcb1233c0b0d
bobyin <bobyin22@gmail.com>
2025-02-05 17:32:57 +0800

    [modify] EP9 整理程式碼 Part1 - base Video 14:00

--- FoodPicker/FoodPicker/ContentView.swift
+++ FoodPicker/FoodPicker/ContentView.swift
@@ -7,116 +7,146 @@
 
 import SwiftUI
 
+extension View {
+    func mainButtonStyle() -> some View {
+        buttonStyle(.borderedProminent)
+        .buttonBorderShape(.capsule)
+        .controlSize(.large)
+    }
+}
+
 
 struct ContentView: View {
-    let food = Food.examples
     @State private var selectedFood: Food?
-    @State private var showInfo: Bool = false
+    @State private var shouldShowInfo: Bool = false
+    
+    let food = Food.examples
     
     var body: some View {
         ScrollView {
             VStack(spacing: 30) {
-                Group {
-                    if selectedFood != .none {
-                        Text("\(selectedFood!.image)")
-                            .font(.system(size: 200))
-                            .minimumScaleFactor(0.1)
-                            .lineLimit(1)
-                    } else {
-                        Image("dinner")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    }
-                }.frame(height: 250)
+                foodImage
                 
+                Text("今天吃什麼？").bold()
                 
-                Text("今天吃什麼？")
-                    .bold()
-                
-                if selectedFood != .none {
-                    HStack {
-                        Text(selectedFood!.name ?? "")
-                            .font(.largeTitle)
-                            .bold()
-                            .foregroundStyle(.green)
-                            .id(selectedFood!.name)
-                            .transition(.asymmetric(
-                                insertion: .opacity
-                                    .animation(.easeInOut(duration: 0.5).delay(0.2)),
-                                removal: .opacity
-                                    .animation(.easeInOut(duration: 0.4))))
-                        Button {
-                            showInfo.toggle()
-                        } label: {
-                            Image(systemName: "info.circle.fill")
-                                .foregroundColor(.secondary)
-                        }.buttonStyle(.plain)
-                    }
-                    
-                    
-                    Text("熱量\(selectedFood!.calorie.formatted())")
-                        .font(.title2)
-                      
-                    
-                VStack {
-                    if showInfo {
-                        Grid(horizontalSpacing: 12, verticalSpacing: 12) {
-                            GridRow {
-                                Text("蛋白質")
-                                Text("脂肪")
-                                Text("碳水")
-                            }.frame(minWidth: 40)
-                            
-                            Divider()
-                                .gridCellUnsizedAxes(.horizontal)
-                                .padding(.horizontal, -10)
-                            
-                            GridRow {
-                                Text(selectedFood!.protein.formatted() + " g")
-                                Text(selectedFood!.calorie.formatted() + " g")
-                                Text(selectedFood!.carb.formatted() + " g")
-                            }
-                        }
-                        .font(.title3)
-                        .padding(.horizontal)
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 8).foregroundColor(Color(.systemBackground)))
-                        .transition(.move(edge: .top).combined(with: .opacity))
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .clipped()
-            }
+                selectedFoodInfoView
                 
                 Spacer().layoutPriority(1)
                 
-                Button {
-                    selectedFood = food.shuffled().first { $0 != selectedFood }
-                } label: {
-                    Text(selectedFood == .none ? "告訴我" : "換一個").frame(width: 200)
-                        .animation(.none, value: selectedFood)
-                        .transformEffect(.identity)
-                }
-                .padding(.bottom, -15)
+                selectFoodButton
                 
-                Button {
-                    selectedFood = .none
-                    showInfo = false
-                } label: {
-                    Text("重置").frame(width: 200, alignment: .center)
-                }
-                .buttonStyle(.bordered)
-                
+                cancelButton
             }
             .padding()
             .frame(maxWidth: .infinity, minHeight: UIScreen.main.bounds.height - 100)
             .font(.title)
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
-            .controlSize(.large)
-            .animation(.spring(dampingFraction: 0.55), value: showInfo)
+            .mainButtonStyle()
+            .animation(.spring(dampingFraction: 0.55), value: shouldShowInfo)
             .animation(.easeInOut(duration: 0.6), value: selectedFood)
         }.background(Color(.secondarySystemBackground))
+    }
+}
+    
+// MARK: - Subviews
+private extension ContentView {
+
+    var foodImage: some View {
+        Group {
+            if selectedFood != .none {
+                Text("\(selectedFood!.image)")
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.1)
+                    .lineLimit(1)
+            } else {
+                Image("dinner")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }.frame(height: 250)
+    }
+    
+    var foodNameView: some View {
+        HStack {
+            Text(selectedFood!.name ?? "")
+                .font(.largeTitle)
+                .bold()
+                .foregroundStyle(.green)
+                .id(selectedFood!.name)
+                .transition(.asymmetric(
+                    insertion: .opacity
+                        .animation(.easeInOut(duration: 0.5).delay(0.2)),
+                    removal: .opacity
+                        .animation(.easeInOut(duration: 0.4))))
+            Button {
+                shouldShowInfo.toggle()
+            } label: {
+                Image(systemName: "info.circle.fill")
+                    .foregroundColor(.secondary)
+            }.buttonStyle(.plain)
+        }
+    }
+    
+    var foodDetailView: some View {
+        VStack {
+            if shouldShowInfo {
+                Grid(horizontalSpacing: 12, verticalSpacing: 12) {
+                    GridRow {
+                        Text("蛋白質")
+                        Text("脂肪")
+                        Text("碳水")
+                    }.frame(minWidth: 40)
+                    
+                    Divider()
+                        .gridCellUnsizedAxes(.horizontal)
+                        .padding(.horizontal, -10)
+                    
+                    GridRow {
+                        Text(selectedFood!.protein.formatted() + " g")
+                        Text(selectedFood!.calorie.formatted() + " g")
+                        Text(selectedFood!.carb.formatted() + " g")
+                    }
+                }
+                .font(.title3)
+                .padding(.horizontal)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 8).foregroundColor(Color(.systemBackground)))
+                .transition(.move(edge: .top).combined(with: .opacity))
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .clipped()
+    }
+    
+    @ViewBuilder var selectedFoodInfoView: some View {
+        if selectedFood != .none {
+            foodNameView
+            
+            Text("熱量\(selectedFood!.calorie.formatted())")
+                .font(.title2)
+            
+            foodDetailView
+        }
+    }
+    
+    var selectFoodButton: some View {
+        Button {
+            selectedFood = food.shuffled().first { $0 != selectedFood }
+        } label: {
+            Text(selectedFood == .none ? "告訴我" : "換一個").frame(width: 200)
+                .animation(.none, value: selectedFood)
+                .transformEffect(.identity)
+        }
+        .padding(.bottom, -15)
+    }
+    
+    var cancelButton: some View {
+        Button {
+            selectedFood = .none
+            shouldShowInfo = false
+        } label: {
+            Text("重置").frame(width: 200, alignment: .center)
+        }
+        .buttonStyle(.bordered)
     }
 }
 
